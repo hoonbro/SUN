e83aea4ec3edbc7f5c7693ae110468db834e3597
multicampus <sun950903@gmail.com>
2021-10-20 14:33:09 +0900

    Fix : conflit 해결중

--- Backend/src/main/java/com/sun/tingle/member/api/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.sun.tingle.member.api.service;
-
-public interface EmailService {
-
-    public void sendId(String email, String memberId);
-
-    public String SendPasswordCode(String email, String name);
-}
- 
