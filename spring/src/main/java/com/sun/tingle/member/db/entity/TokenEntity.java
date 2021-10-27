beeaf3934cf5321399b6ec4019de51d562ac40c9
변지훈 <wlgns1588@naver.com>
2021-10-27 13:30:51 +0900

    token table 추가

---
+++ spring/src/main/java/com/sun/tingle/member/db/entity/TokenEntity.java
@@ -0,0 +1,28 @@
+package com.sun.tingle.member.db.entity;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+@Builder
+@Table(name = "token")
+public class TokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키 생성을 db에 위임하는 방법(auto increment)
+    private Long id;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
+
+    @Column(name = "mid")
+    private Long mid;
+
+    @Column(name = "expire_time")
+    private Long expireTime;
+}
