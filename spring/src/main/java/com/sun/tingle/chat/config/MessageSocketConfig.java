f0ad3b1465e81e0130b75c59f2386aa8fdc08811
ilminmoon <ian.ilminmoon@gmail.com>
2021-10-27 17:47:26 +0900

    kafkaconfig udpate

--- spring/src/main/java/com/sun/tingle/chat/config/MessageSocketConfig.java
+++ spring/src/main/java/com/sun/tingle/chat/config/MessageSocketConfig.java
@@ -1,4 +1,38 @@
 package com.sun.tingle.chat.config;
 
-public class MessageSocketConfig {
+import com.sun.tingle.chat.config.handler.StompHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker
+public class MessageSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final StompHandler stompHandler;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/room");
+        registry.setApplicationDestinationPrefixes("/message");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS()
+                .setStreamBytesLimit(512 * 1024)
+                .setHttpMessageCacheSize(1000)
+                .setDisconnectDelay(30 * 1000);
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
+    }
 }
