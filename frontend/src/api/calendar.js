e13e31acd7679569d36d4bfb9eb0dac7d0ba67c3
byunghun-jake <figma@kakao.com>
2021-11-10 11:02:55 +0900

    feat: 공유 캘린더 추가 구현

--- frontend/src/api/calendar.js
+++ frontend/src/api/calendar.js
@@ -5,6 +5,16 @@ const createCalendar = async (calendarName) => {
   return res.data
 }
 
+const addShareCalendar = async (calendarCode) => {
+  const res = await client.post(`calendar/share`, { calendarCode })
+  return res.data
+}
+
+const getAllCalendarList = async () => {
+  const res = await client.get("/calendar/every/calendars")
+  return res.data
+}
+
 const getCalendar = async (calendarCode) => {
   const res = await client.get(`calendar/${calendarCode}`)
   return res.data
@@ -13,6 +23,14 @@ const getCalendar = async (calendarCode) => {
 const editCalendar = async ({ calendarCode, calendarName }) => {
   const res = await client.put(`calendar/${calendarCode}`, { calendarName })
   return res.data
+}
+
+const deleteMyCalendar = async (calendarCode) => {
+  await client.delete(`calendar/${calendarCode}`)
+}
+
+const deleteShareCalendar = async (calendarCode) => {
+  await client.delete(`calendar/share/${calendarCode}`)
 }
 
 const getMissionList = async (params) => {
@@ -24,8 +42,12 @@ const getMissionList = async (params) => {
 
 const calendarAPI = {
   createCalendar,
+  addShareCalendar,
+  getAllCalendarList,
   getCalendar,
   editCalendar,
+  deleteMyCalendar,
+  deleteShareCalendar,
   getMissionList,
 }
 
