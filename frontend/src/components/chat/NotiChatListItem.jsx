57f546877773f8fdb9387c35ebb5f2ad0c35c597
byunghun-jake <figma@kakao.com>
2021-11-17 11:46:41 +0900

    update: 채팅 바로가기 구현

--- frontend/src/components/chat/NotiChatListItem.jsx
+++ frontend/src/components/chat/NotiChatListItem.jsx
@@ -1,18 +1,10 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import React, { useMemo } from "react"
 import moment from "moment"
 import "moment/locale/ko"
 import gravatar from "gravatar"
-import { IoEllipsisVertical } from "react-icons/io5"
 import { Link } from "react-router-dom"
 
 const NotiChatListItem = ({ ...chat }) => {
-  const [menuOpen, setMenuOpen] = useState(false)
-  const deleteBtnEl = useRef()
-
-  const handleMenuToggle = useCallback(() => {
-    setMenuOpen((prevState) => !prevState)
-  }, [])
-
   const formattedTime = useMemo(() => {
     switch (moment(chat.sentTime).diff(moment(Date.now()), "days")) {
       case 0: {
@@ -30,18 +22,11 @@ const NotiChatListItem = ({ ...chat }) => {
       : gravatar.url(chat?.email, { d: "retro" })
   }, [chat])
 
-  useEffect(() => {
-    if (menuOpen === true) {
-      deleteBtnEl.current.focus()
-    }
-  }, [deleteBtnEl, menuOpen])
-
   return (
     <li>
       <Link
         className="p-2 rounded grid gap-1 hover:bg-gray-50"
-        // to={`/calendars/${chat.chatRoom.mission.calendarCode}/events/${chat.chatRoom.mission.missionId}`}
-        to={`/calendars`}
+        to={`/calendars/${chat.calendarId}/events/${chat.missionId}`}
       >
         <div className="flex gap-2">
           <div className="img-wrapper w-12 h-12 rounded-full overflow-hidden">
@@ -54,22 +39,6 @@ const NotiChatListItem = ({ ...chat }) => {
           <div className="flex-1 grid gap-1">
             <p className="font-bold">{chat.nickname}</p>
             <p className="text-sm font-medium">{chat.content}</p>
-          </div>
-          <div className="relative">
-            <button className="flex" onClick={handleMenuToggle}>
-              <IoEllipsisVertical size={18} />
-            </button>
-            {menuOpen && (
-              <div className="absolute bottom-0 right-0 bg-white w-12 shadow-lg rounded">
-                <button
-                  className="text-sm font-medium text-red-500 py-1 px-2 w-full"
-                  onBlur={handleMenuToggle}
-                  ref={deleteBtnEl}
-                >
-                  삭제
-                </button>
-              </div>
-            )}
           </div>
         </div>
         <div className="flex items-center justify-between">
